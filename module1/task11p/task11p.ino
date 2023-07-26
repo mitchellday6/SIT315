9260e80655116ba4fce81116c968ff2dabe72bc1
mitchellday6 <mitchell.day.6@gmail.com>
2023-07-26 23:49:11 +1000

    Update task11p.ino

--- module1/task11p/task11p.ino
+++ module1/task11p/task11p.ino
@@ -1,12 +1,12 @@
 // C++ code
 //
 int sensorValue = 0;
-int GREEN_LIGHT = 12;
+int GREEN_LIGHT = 8;
 
 void setup()
 {
   pinMode(A0, INPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT); 
   pinMode(GREEN_LIGHT, OUTPUT);
   Serial.begin(9600);
 }
@@ -15,19 +15,22 @@ void loop()
 {
   // read the value from the sensor
   sensorValue = analogRead(A0);
-  Serial.println("Attentutor Input: "+sensorValue);
+  Serial.print("Attentutor Input: ");
+  Serial.println(sensorValue);
   if(sensorValue == 1023){
     Serial.println("Attentutor Max");
     digitalWrite(LED_BUILTIN, HIGH);
   } else {
+    digitalWrite(LED_BUILTIN, LOW);
     // turn the LED on
-    Serial.println("Light Delay: "+1024-sensorValue);
+    Serial.print("Light Delay: ");
+    Serial.println(1024-sensorValue);
     digitalWrite(GREEN_LIGHT, HIGH);
     // pause the program for <sensorValue> millseconds
     delay(1024- sensorValue); // Wait for sensorValue millisecond(s)
     // turn the LED off
     digitalWrite(GREEN_LIGHT, LOW);
     // pause the program for <sensorValue> millseconds
-    delay(sensorValue); // Wait for sensorValue millisecond(s)
+    delay(1024-sensorValue); // Wait for sensorValue millisecond(s)
   }
 }
